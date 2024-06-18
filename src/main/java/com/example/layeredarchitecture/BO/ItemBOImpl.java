01e3b49c0791022b0ed11690b8ec3e1f8227ed93
Sanduni S Priyadarshani <priyadarshanissp@gmail.com>
2024-06-18 20:50:17 +0530

    layered Achitecture

--- src/main/java/com/example/layeredarchitecture/BO/ItemBOImpl.java
+++ src/main/java/com/example/layeredarchitecture/BO/ItemBOImpl.java
@@ -1,5 +1,7 @@
 package com.example.layeredarchitecture.BO;
 
+import com.example.layeredarchitecture.dao.Custom.Impl.ItemDAOImpl;
+import com.example.layeredarchitecture.dao.Custom.ItemDAO;
 import com.example.layeredarchitecture.dao.SqlUtill;
 import com.example.layeredarchitecture.model.ItemDTO;
 
@@ -9,26 +11,38 @@ import java.util.ArrayList;
 
 public class ItemBOImpl {
     public ArrayList<ItemDTO> getAll() throws SQLException, ClassNotFoundException {
-
+        ItemDAO itemDAO= new ItemDAOImpl();
+        return itemDAO.getAll();
     }
 
     public boolean delete(String code) throws SQLException, ClassNotFoundException {
-
+        ItemDAO itemDAO= new ItemDAOImpl();
+        return itemDAO.delete(code);
     }
 
     public boolean add(ItemDTO dto) throws SQLException, ClassNotFoundException {
+        ItemDAO itemDAO= new ItemDAOImpl();
+        return itemDAO.add(dto);
          }
 
     public boolean update(ItemDTO dto) throws SQLException, ClassNotFoundException {
+        ItemDAO itemDAO= new ItemDAOImpl();
+        return itemDAO.update(dto);
         }
 
     public boolean exist(String code) throws SQLException, ClassNotFoundException {
+        ItemDAO itemDAO= new ItemDAOImpl();
+        return itemDAO.exist(code);
       }
 
     public String generateNewID() throws SQLException, ClassNotFoundException {
+        ItemDAO itemDAO= new ItemDAOImpl();
+        return itemDAO.generateNewID();
     }
 
     public ItemDTO search(String code) throws SQLException, ClassNotFoundException {
+        ItemDAO itemDAO= new ItemDAOImpl();
+        return itemDAO.search(code);
         }
 
 
