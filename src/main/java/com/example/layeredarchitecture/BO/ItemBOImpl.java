7a2c884719f34ec83ded138a92d97e15aa19c960
Sanduni S Priyadarshani <priyadarshanissp@gmail.com>
2024-06-18 20:58:19 +0530

    layered Achitecture

--- src/main/java/com/example/layeredarchitecture/BO/ItemBOImpl.java
+++ src/main/java/com/example/layeredarchitecture/BO/ItemBOImpl.java
@@ -9,39 +9,40 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class ItemBOImpl {
+public class ItemBOImpl implements ItemBO{
+    ItemDAO itemDAO= new ItemDAOImpl();
     public ArrayList<ItemDTO> getAll() throws SQLException, ClassNotFoundException {
-        ItemDAO itemDAO= new ItemDAOImpl();
+        /*ItemDAO itemDAO= new ItemDAOImpl();*/
         return itemDAO.getAll();
     }
 
     public boolean delete(String code) throws SQLException, ClassNotFoundException {
-        ItemDAO itemDAO= new ItemDAOImpl();
+       /* ItemDAO itemDAO= new ItemDAOImpl();*/
         return itemDAO.delete(code);
     }
 
     public boolean add(ItemDTO dto) throws SQLException, ClassNotFoundException {
-        ItemDAO itemDAO= new ItemDAOImpl();
+        /*ItemDAO itemDAO= new ItemDAOImpl();*/
         return itemDAO.add(dto);
          }
 
     public boolean update(ItemDTO dto) throws SQLException, ClassNotFoundException {
-        ItemDAO itemDAO= new ItemDAOImpl();
+        /*ItemDAO itemDAO= new ItemDAOImpl();*/
         return itemDAO.update(dto);
         }
 
     public boolean exist(String code) throws SQLException, ClassNotFoundException {
-        ItemDAO itemDAO= new ItemDAOImpl();
+       /* ItemDAO itemDAO= new ItemDAOImpl();*/
         return itemDAO.exist(code);
       }
 
     public String generateNewID() throws SQLException, ClassNotFoundException {
-        ItemDAO itemDAO= new ItemDAOImpl();
+        /*ItemDAO itemDAO= new ItemDAOImpl();*/
         return itemDAO.generateNewID();
     }
 
     public ItemDTO search(String code) throws SQLException, ClassNotFoundException {
-        ItemDAO itemDAO= new ItemDAOImpl();
+        /*ItemDAO itemDAO= new ItemDAOImpl();*/
         return itemDAO.search(code);
         }
 
